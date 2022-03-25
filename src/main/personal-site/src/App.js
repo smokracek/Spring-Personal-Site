03efd19ce1769c3b31331c7bfa2a48818a79ef3b
Sam Mokracek <samuelmokracek@gmail.com>
2022-03-24 23:07:40 -0700

    Initial commit

---
+++ src/main/personal-site/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="App"
+      class="flex flex-col justify-center items-center w-screen h-screen bg-gray-200"
+    >
+      <p class="text-white text-center text-9xl font-serif italic drop-shadow-md">
+        It works!
+      </p>
+    </div>
+  );
+}
+
+export default App;
