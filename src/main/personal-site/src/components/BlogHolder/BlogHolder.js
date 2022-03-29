009b95cfc975e4024cf29b100bf2060ac6af5892
Sam Mokracek <samuelmokracek@gmail.com>
2022-03-28 23:28:30 -0700

    Made some template blog posts

--- src/main/personal-site/src/components/BlogHolder/BlogHolder.js
+++ src/main/personal-site/src/components/BlogHolder/BlogHolder.js
@@ -3,7 +3,111 @@
 
 function BlogHolder() {
   return (
-    <div class="flex flex-col w-full h-auto bg-themeDarkBlue m-5 ml-0 shadow-lg"></div>
+    <div class="flex flex-col w-full h-auto bg-themeDarkBlue m-5 ml-0 overflow-y-auto overscroll-y-none">
+      {/* This is one blog post */}
+      <div class="w-auto h-auto bg-themeBlack m-4 shadow-xl p-4">
+        <p class="text-4xl text-themeRed mb-2">Blog Title</p>
+        <p class="text-themeText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui
+          metus, convallis quis purus sed, commodo accumsan erat. Praesent dolor
+          justo, fermentum vel suscipit non, fringilla tincidunt magna.
+          Suspendisse ac est porttitor diam sagittis volutpat sit amet quis
+          tellus. Morbi luctus tellus id enim cursus, ornare sagittis diam
+          molestie. Suspendisse ac arcu non velit pharetra vehicula eget
+          lobortis magna. Suspendisse potenti. Aliquam id tempor nunc, ac
+          dignissim mi. Vestibulum suscipit felis quis justo cursus ornare.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui
+          metus, convallis quis purus sed, commodo accumsan erat. Praesent dolor
+          justo, fermentum vel suscipit non, fringilla tincidunt magna.
+          Suspendisse ac est porttitor diam sagittis volutpat sit amet quis
+          tellus. Morbi luctus tellus id enim cursus, ornare sagittis diam
+          molestie. Suspendisse ac arcu non velit pharetra vehicula eget
+          lobortis magna. Suspendisse potenti. Aliquam id tempor nunc, ac
+          dignissim mi. Vestibulum suscipit felis quis justo cursus ornare.
+        </p>
+        <p class="text-2xl text-themeGreen mt-2">Sub Heading</p>
+        <p class="text-themeText mt-2">
+          Phasellus non nulla ipsum. Nam vitae massa ultricies, volutpat orci
+          ac, dapibus mi. Suspendisse non metus sem. Aliquam sit amet orci id
+          metus ullamcorper sagittis in nec felis. Duis congue mi mi. Vestibulum
+          finibus et tellus at iaculis. Vestibulum ante ipsum primis in faucibus
+          orci luctus et ultrices posuere cubilia curae; Nullam maximus
+          consectetur sem, non ultrices metus porta sit amet.
+        </p>
+      </div>
+
+      {/* This is one blog post */}
+      <div class="w-auto h-auto bg-themeBlack m-4 shadow-xl p-4">
+        <p class="text-4xl text-themeRed mb-2">Blog Title</p>
+        <p class="text-themeText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui
+          metus, convallis quis purus sed, commodo accumsan erat. Praesent dolor
+          justo, fermentum vel suscipit non, fringilla tincidunt magna.
+          Suspendisse ac est porttitor diam sagittis volutpat sit amet quis
+          tellus. Morbi luctus tellus id enim cursus, ornare sagittis diam
+          molestie. Suspendisse ac arcu non velit pharetra vehicula eget
+          lobortis magna. Suspendisse potenti. Aliquam id tempor nunc, ac
+          dignissim mi. Vestibulum suscipit felis quis justo cursus ornare.
+        </p>
+        <p class="text-2xl text-themeGreen mt-2">Sub Heading</p>
+        <p class="text-themeText mt-2">
+          Phasellus non nulla ipsum. Nam vitae massa ultricies, volutpat orci
+          ac, dapibus mi. Suspendisse non metus sem. Aliquam sit amet orci id
+          metus ullamcorper sagittis in nec felis. Duis congue mi mi. Vestibulum
+          finibus et tellus at iaculis. Vestibulum ante ipsum primis in faucibus
+          orci luctus et ultrices posuere cubilia curae; Nullam maximus
+          consectetur sem, non ultrices metus porta sit amet.
+        </p>
+      </div>
+
+      {/* This is one blog post */}
+      <div class="w-auto h-auto bg-themeBlack m-4 shadow-xl p-4">
+        <p class="text-4xl text-themeRed mb-2">Blog Title</p>
+        <p class="text-themeText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui
+          metus, convallis quis purus sed, commodo accumsan erat. Praesent dolor
+          justo, fermentum vel suscipit non, fringilla tincidunt magna.
+          Suspendisse ac est porttitor diam sagittis volutpat sit amet quis
+          tellus. Morbi luctus tellus id enim cursus, ornare sagittis diam
+          molestie. Suspendisse ac arcu non velit pharetra vehicula eget
+          lobortis magna. Suspendisse potenti. Aliquam id tempor nunc, ac
+          dignissim mi. Vestibulum suscipit felis quis justo cursus ornare.
+        </p>
+        <p class="text-2xl text-themeGreen mt-2">Sub Heading</p>
+        <p class="text-themeText mt-2">
+          Phasellus non nulla ipsum. Nam vitae massa ultricies, volutpat orci
+          ac, dapibus mi. Suspendisse non metus sem. Aliquam sit amet orci id
+          metus ullamcorper sagittis in nec felis. Duis congue mi mi. Vestibulum
+          finibus et tellus at iaculis. Vestibulum ante ipsum primis in faucibus
+          orci luctus et ultrices posuere cubilia curae; Nullam maximus
+          consectetur sem, non ultrices metus porta sit amet.
+        </p>
+      </div>
+
+      {/* This is one blog post */}
+      <div class="w-auto h-auto bg-themeBlack m-4 shadow-xl p-4">
+        <p class="text-4xl text-themeRed mb-2">Blog Title</p>
+        <p class="text-themeText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui
+          metus, convallis quis purus sed, commodo accumsan erat. Praesent dolor
+          justo, fermentum vel suscipit non, fringilla tincidunt magna.
+          Suspendisse ac est porttitor diam sagittis volutpat sit amet quis
+          tellus. Morbi luctus tellus id enim cursus, ornare sagittis diam
+          molestie. Suspendisse ac arcu non velit pharetra vehicula eget
+          lobortis magna. Suspendisse potenti. Aliquam id tempor nunc, ac
+          dignissim mi. Vestibulum suscipit felis quis justo cursus ornare.
+        </p>
+        <p class="text-2xl text-themeGreen mt-2">Sub Heading</p>
+        <p class="text-themeText mt-2">
+          Phasellus non nulla ipsum. Nam vitae massa ultricies, volutpat orci
+          ac, dapibus mi. Suspendisse non metus sem. Aliquam sit amet orci id
+          metus ullamcorper sagittis in nec felis. Duis congue mi mi. Vestibulum
+          finibus et tellus at iaculis. Vestibulum ante ipsum primis in faucibus
+          orci luctus et ultrices posuere cubilia curae; Nullam maximus
+          consectetur sem, non ultrices metus porta sit amet.
+        </p>
+      </div>
+    </div>
   );
 }
 
